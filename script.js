1d388e1e0cedc4c6367fd861cfbe405659782af8
Dip sadhu <sadhudip212@gmail.com>
2026-03-12 23:02:22 +0530

    first commit. working on js

---
+++ script.js
@@ -0,0 +1,98 @@
+function createPlayer(name, marker) {
+
+    function getName() {
+        return name;
+    }
+
+    function getMarker() {
+        return marker;
+    }
+
+    return {
+        name,
+        marker,
+        getName,
+        getMarker
+    }
+}
+
+const gameBoard = (function () {
+    const board = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ];
+
+    function getBoard() {
+        return board;
+    }
+
+    function placeMark(position, marker) {
+        if (board[position] == "") {
+            board[position] = marker;
+            console.log("move successful");
+            return true;
+        } else {
+            console.log("invalid move!");
+            return false;
+        }
+    }
+
+    function resetBoard() {
+        for (let i = 0; i < 9; i++) {
+            board[i] = "";
+        }
+    }
+
+    return { getBoard, placeMark, resetBoard };
+})();
+
+const gameController = (function () {
+    const player1 = createPlayer("desk", "X");
+    const player2 = createPlayer("disk", "O");
+
+    let currentPlayer = player1;
+
+    function getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    function switchPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+    function playRound(position) {
+        console.log("current player: ", currentPlayer.getName());
+
+        //gameBoard.placeMark(position, currentPlayer.getMarker());
+        const moveSuccess = gameBoard.placeMark(position, currentPlayer.getMarker());
+
+        if (moveSuccess) {
+            
+
+            switchPlayer();
+        }
+
+
+    }
+
+    function checkWin() {
+        const winningCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+    }
+
+    return { getCurrentPlayer, switchPlayer, playRound };
+})();
